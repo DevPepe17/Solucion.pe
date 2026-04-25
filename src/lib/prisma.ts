88dfd6cd07e55725a09e8e2005cca5cf0957f700
DevPepe17 <jgallardo.jfg.17@gmail.com>
2026-04-25 18:46:01 -0500

    Fix: add prisma generate to build and postinstall for Vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,6 @@
 import { Pool } from 'pg'
 import { PrismaPg } from '@prisma/adapter-pg'
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaClient } = require('@prisma/client')
+import { PrismaClient } from '@prisma/client'
 
 const connectionString = `${process.env.DATABASE_URL}`
 
